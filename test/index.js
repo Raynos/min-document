4cb95ae7f2c84fa0284bcaf00f6e253fd05d64a0
Lauri Rooden <lauri@rooden.ee>
2013-07-30 23:29:22 +0300

    Add document.getElementById

--- test/index.js
+++ test/index.js
@@ -50,3 +50,36 @@ test("can createDocumentFragment", function (assert) {
 })
 
 
+test("can getElementById", function (assert) {
+
+    function append_div(id, parent) {
+        var div = document.createElement("div")
+        div.id = id
+        parent.appendChild(div)
+        return div
+    }
+
+    var div1   = append_div(1, document.body)
+    var div2   = append_div(2, document.body)
+    var div3   = append_div(3, document.body)
+
+    var div11  = append_div(11, div1)
+    var div12  = append_div(12, div1)
+    var div21  = append_div(21, div2)
+    var div22  = append_div(22, div2)
+    var div221 = append_div(221, div22)
+    var div222 = append_div(222, div22)
+
+    assert.equal(document.getElementById(1),    div1)
+    assert.equal(document.getElementById("2"),  div2)
+    assert.equal(document.getElementById(3),    div3)
+    assert.equal(document.getElementById(11),   div11)
+    assert.equal(document.getElementById(12),   div12)
+    assert.equal(document.getElementById(21),   div21)
+    assert.equal(document.getElementById(22),   div22)
+    assert.equal(document.getElementById(221),  div221)
+    assert.equal(document.getElementById(222),  div222)
+
+    assert.end()
+})
+
