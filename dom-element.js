fe1cfac7e283df534658fdaa80ab2c02862b069a
Matt-Esch <matt@mattesch.info>
2014-04-14 06:11:13 +0100

    added insertBefore and fixed parentNode on insert/remove

--- dom-element.js
+++ dom-element.js
@@ -27,23 +27,59 @@ DOMElement.prototype.type = "DOMElement"
 DOMElement.prototype.nodeType = 1
 
 DOMElement.prototype.appendChild = function _Element_appendChild(child) {
+    if (child.parentNode) {
+        child.parentNode.removeChild(child)
+    }
+
     this.childNodes.push(child)
     child.parentNode = this
 }
 
 DOMElement.prototype.replaceChild =
     function _Element_replaceChild(elem, needle) {
+        // TODO: Throw NotFoundError if needle.parentNode !== this
+
+        if (elem.parentNode) {
+            elem.parentNode.removeChild(elem)
+        }
+
         var index = this.childNodes.indexOf(needle)
 
+        needle.parentNode = null
         this.childNodes[index] = elem
         elem.parentNode = this
     }
 
 DOMElement.prototype.removeChild = function _Element_removeChild(elem) {
+    // TODO: Throw NotFoundError if elem.parentNode !== this
+
     var index = this.childNodes.indexOf(elem)
 
+    this.childNodes[index].parentNode = null
     this.childNodes.splice(index, 1)
 }
+
+DOMElement.prototype.insertBefore =
+    function _Element_insertBefore(newElement, referenceElement) {
+        // TODO: Throw NotFoundError if referenceElement is a dom node
+        // and parentNode !== this
+
+        if (newElement.parentNode) {
+            newElement.parentNode.removeChild(newElement)
+        }
+
+        var index = referenceElement ?
+            this.childNodes.indexOf(referenceElement) :
+            -1
+
+        if (index > -1) {
+            this.childNodes.splice(index, 0, newElement)
+        } else {
+            this.childNodes.push(newElement)
+        }
+
+        newElement.parentNode = this
+    }
 
 DOMElement.prototype.addEventListener = addEventListener
 DOMElement.prototype.dispatchEvent = dispatchEvent
