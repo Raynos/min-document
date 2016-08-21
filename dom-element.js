9ace94a91d7dbda232fc3a95da08e3ab8abba68c
Scott Reynen <scott@slicedbreadlabs.com>
2016-08-21 05:31:27 -0600

    Update dom-element.js

--- dom-element.js
+++ dom-element.js
@@ -16,7 +16,6 @@ function DOMElement(tagName, owner, namespace) {
     var ns = namespace === undefined ? htmlns : (namespace || null)
 
     this.tagName = ns === htmlns ? String(tagName).toUpperCase() : tagName
-    this.nodeName = this.tagName
     this.className = ""
     this.dataset = {}
     this.childNodes = []
@@ -27,7 +26,7 @@ function DOMElement(tagName, owner, namespace) {
     this._attributes = {}
 
     if (this.tagName === 'INPUT') {
-      this.setAttribute('type', 'text')
+      this.type = 'text';
     }
 }
 
@@ -104,18 +103,25 @@ DOMElement.prototype.setAttributeNS =
             prefix = name.substr(0, colonPosition)
             localName = name.substr(colonPosition + 1)
         }
-        var attributes = this._attributes[namespace] || (this._attributes[namespace] = {})
-        attributes[localName] = {value: value, prefix: prefix}
+        if (this.tagName === 'INPUT' && name === 'type') {
+          this.type = value;
+        }
+        else {
+          var attributes = this._attributes[namespace] || (this._attributes[namespace] = {})
+          attributes[localName] = {value: value, prefix: prefix}
+        }
     }
 
 DOMElement.prototype.getAttributeNS =
     function _Element_getAttributeNS(namespace, name) {
         var attributes = this._attributes[namespace];
         var value = attributes && attributes[name] && attributes[name].value
+        if (this.tagName === 'INPUT' && name === 'type') {
+          return this.type;
+        }
         if (typeof value !== "string") {
             return null
         }
-
         return value
     }
 
